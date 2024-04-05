dff780acadbe2c88593ba521a2c0d32ef463b602
alastor- <alexismhgdotanime@gmail.com>
2024-04-05 17:21:51 -0400

    IndecisionApp finished

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,6 @@ import IndecisionBase from '@/components/IndecisionBase.vue';
 </script>
 
 <template>
-  <!-- <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-  </header> -->
   <main>
     <!-- <Counter msg="Contador1" :start="10" /> -->
     <IndecisionBase />
--- src/components/IndecisionBase.vue
+++ src/components/IndecisionBase.vue
@@ -1,12 +1,12 @@
 <template>
-    <img :src="image" alt="Background">
+    <img v-if="image" :src="image" alt="Background">
 
     <div class="indecision-container">
         <input v-model="question" type="text" placeholder="Hazme una pregunta">
         <p>Recuerda terminar con un signo de interrogación (?)</p>
     </div>
 
-    <div>
+    <div v-if="isValidQuestion">
         <h2>{{ question }}</h2>
         <h1>{{ answer }}</h1>
     </div>
@@ -18,7 +18,8 @@ export default {
         return {
             question: null,
             answer: null,
-            img: null
+            image: null,
+            isValidQuestion: false
         }
     },
     methods: {
@@ -26,13 +27,28 @@ export default {
             this.answer = "Pensando..."
             const { answer, image } = await fetch("https://yesno.wtf/api").then(r => r.json());
 
+            switch (answer) {
+                case "yes":
+                    this.answer = "Si";
+                    break;
+                case "maybe":
+                    this.answer = "Tal vez";
+                    break;
+                case "no":
+                    this.answer = "No";
+                    break;
+                default:
+                    break;
+            }
             this.answer = answer;
             this.image = image;
         }
     },
     watch: {
         question(value) {
+            this.isValidQuestion = false;
             if (!value.includes("?")) return;
+            this.isValidQuestion = true;
             this.getAnswer();
         }
     }
@@ -52,12 +68,13 @@ img {
 
 .indecision-container {
     position: relative;
-    z-index: 90;
+    z-index: 1;
 }
 
 input {
     width: 250px;
     padding: 10px 15px;
+    margin: 25px 0;
     border-radius: 5px;
     border: none;
 }
